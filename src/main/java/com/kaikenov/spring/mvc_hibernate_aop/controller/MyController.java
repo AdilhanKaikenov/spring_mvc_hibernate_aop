354bd9c6e46197f4cd8375d536ea66ea9af5328f
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-12-29 12:26:59 +0600

    add a new employee

--- src/main/java/com/kaikenov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/kaikenov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -1,11 +1,11 @@
 package com.kaikenov.spring.mvc_hibernate_aop.controller;
 
-import com.kaikenov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.kaikenov.spring.mvc_hibernate_aop.entity.Employee;
 import com.kaikenov.spring.mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,5 +24,20 @@ public class MyController {
         return "all-employees";
     }
 
+    @RequestMapping("/addNewEmpoyee")
+    public String addNewEmployee(Model model) {
+        Employee employee = new Employee();
+
+        model.addAttribute("employee", employee);
+
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveNewEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+
+        return "redirect:/";
+    }
 
 }
